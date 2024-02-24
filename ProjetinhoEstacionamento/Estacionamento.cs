25da8c8f03e10bc86f5c4c592eb865c425721c5d
edson.cordeiro <edson.cordeiro@CECSECORDEIRON>
2024-02-24 14:05:52 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetinhoEstacionamento/Estacionamento.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetinhoEstacionamento
+{
+    public class ManipuladorArquivoDeTexto(string? manipuladorArquivo)
+    {
+        private readonly string? manipuladorArquivo = manipuladorArquivo;
+
+        // METODO PARA ADICIONAR
+        public void AdicionarStringAoArquivo(string texto)
+        {
+            //TRY VAI VERIFICAR SE A STRING FOI ADIOCIONADA COM SUCESSO OU NÃO
+            try
+            {
+                using (StreamWriter escrever = new(manipuladorArquivo, true))
+                {
+                    escrever.WriteLine(texto);
+                }
+                Console.WriteLine("Placa Adicionada com Sucesso");
+            } 
+            //CATH VAI APRESENTAR A MENSAGEM DA EXCEPTION PRO CÓDIGO NÃO QUEBRAR
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao adicionar a placa no arquivo {ex.Message}");
+            }
+        }
+
+        public void RemoverStringDoArquivo(string texto)
+        {
+            try
+            {
+                List<string> linhas = new(File.ReadLines(manipuladorArquivo));
+                linhas.Remove(texto);
+                File.WriteAllLines(manipuladorArquivo, linhas);
+                Console.WriteLine("String da placa removida com sucesso do arquivo");
+            } catch (Exception ex)
+            {
+                Console.WriteLine($"A String não pôde ser removida {ex.Message}");
+            }
+        }
+
+        public void ImprimirConteudoArquivo()
+        {
+            try
+            {
+                string[] linhas = File.ReadAllLines(manipuladorArquivo);
+                Console.WriteLine("\nLista de veículos estacionados \n");
+                foreach (string linha in linhas) 
+                {
+                    Console.WriteLine(linha);
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine($"Falha ao imprimir{ex.Message}");
+            }
+        }
+
+    }
+}
---
+++ ProjetinhoEstacionamento/Program.cs
@@ -0,0 +1,73 @@
+﻿using ProjetinhoEstacionamento;
+
+Console.WriteLine("Bem-Vindo ao estacionamento dos CodaRuim");
+Console.WriteLine("----------------------------------------");
+Console.WriteLine("--O valor inicial é de R$5 + R$3 a hora--\n");
+Console.WriteLine("Você vai estacionar? S/N");
+string? continua = Console.ReadLine();
+
+string caminhoDoArquivo = "C:\\Users\\edson.cordeiro\\source\\repos\\ProjetinhoEstacionamento\\ProjetinhoEstacionamento\\PlacasArmazenadas.txt";
+ManipuladorArquivoDeTexto manipuladorArquivo = new(caminhoDoArquivo);
+
+if (continua == "S" || continua == "s")
+{
+
+    while (true)
+    {
+        Console.WriteLine("Escolha uma dasopções abaixo");
+        Console.WriteLine("1 - Adicionar placa");
+        Console.WriteLine("2 - Remover placa");
+        Console.WriteLine("3 - Veículos estacionados");
+        Console.WriteLine("4 - Sair");
+
+        string? opcao = Console.ReadLine();
+
+        switch (opcao)
+        {
+            case "1":
+                Console.WriteLine("Digite a placa que deseja adicionar");
+                string? adicionarPlaca = Console.ReadLine();
+                manipuladorArquivo.AdicionarStringAoArquivo(adicionarPlaca);
+                Console.Clear();
+                break;
+
+            case "2":
+                Console.WriteLine("Quanto tempo você ficou estacionado? ");
+                int tempoEstacionado = Convert.ToInt32(Console.ReadLine());
+                int valorAPagar;
+                if (tempoEstacionado > 1)
+                {
+                    valorAPagar = 5 + (tempoEstacionado * 3);
+                }
+                else valorAPagar = 5;
+                Console.WriteLine($"O valor a pagar é de: {valorAPagar}");
+                Console.WriteLine("Digite aqui a placa que deseja remover");
+                string? removerPlaca = Console.ReadLine();
+                manipuladorArquivo.RemoverStringDoArquivo(removerPlaca);
+                Console.Clear();
+                break;
+
+            case "3":
+                manipuladorArquivo.ImprimirConteudoArquivo();
+                Console.WriteLine("\n");
+                break;
+
+            case "4":
+                Console.WriteLine("Sair");
+                return;
+            default:
+                Console.WriteLine("Escolha uma opção válida");
+                break;
+        }
+
+    }
+}
+
+if (continua == "N" || continua == "n")
+{
+    Console.WriteLine("Tudo bem então, estamos disponíveís caso deseje estacionar");
+}
+else
+{
+    Console.WriteLine("Opção Inválida");
+}
